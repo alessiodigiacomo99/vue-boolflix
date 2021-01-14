c059df6d845f190551bcd3fe2ae204c879ac4ec6
alessio <alessiodg99.adg@gmail.com>
2021-01-14 12:48:38 +0100

    fine milestone 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,49 @@
 let app = new Vue({
     el: '#root',
     data:{
-        film:"",
+        film:"pa",
         films:[],
         input: document.getElementById("input"),
         series:[],
     },
     methods:{
+        manipolazioniElemento(elemento){
+            elemento.vote_average = Math.ceil(elemento.vote_average / 2);
+            if(elemento.original_language == "en"){
+                elemento.original_language = "gb"; 
+            }else if(elemento.original_language == "ja"){
+                elemento.original_language = "jp";
+            };
+            if (elemento.overview.length >= 100) {
+                elemento.overview = elemento.overview.slice(0, 100) + "...";
+            }
+        },
         searchFilms(){
             if(this.film.length > 0){
                 axios.get(`https://api.themoviedb.org/3/search/movie?api_key=447ce90d24c1fcb65e78b03135e2905b&query=${this.film}`)
                 .then(response =>{
                     this.films = response.data.results;
                     this.films.forEach(element => {
-                        element.vote_average = Math.ceil(element.vote_average / 2);
-                        if(element.original_language == "en"){
-                            element.original_language = "gb"; 
-                        }else if(element.original_language == "ja"){
-                            element.original_language = "jp";
-                        };
-                        if (element.overview.length >= 100) {
-                            element.overview = element.overview.slice(0, 100) + "...";
-                        }
-                    });
-                    axios.get(`https://api.themoviedb.org/3/search/tv/?api_key=447ce90d24c1fcb65e78b03135e2905b&query=${this.film}`)
-                    .then(response =>{
-                        this.series = response.data.results;
-                        this.series.forEach(element => {
-                            element.vote_average = Math.ceil(element.vote_average / 2);
-                            if(element.original_language == "en"){
-                                element.original_language = "gb";
-                            }else if(element.original_language == "ja"){
-                                element.original_language = "jp";
-                            }
-                        });
+                        this.manipolazioniElemento(element);
                     })
                 })
             }
         },
+        searchSeries(){
+            axios.get(`https://api.themoviedb.org/3/search/tv/?api_key=447ce90d24c1fcb65e78b03135e2905b&query=${this.film}`)
+            .then(response =>{
+                this.series = response.data.results;
+                this.series.forEach(element => {
+                    this.manipolazioniElemento(element);
+                })
+            })
+        },
+        search(){
+            this.searchFilms();
+            this.searchSeries();
+        }
     },
     mounted(){
-        this.searchFilms();
+        this.search();
     }
 })
